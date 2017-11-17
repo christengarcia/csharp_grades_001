b324cea45608bf0ee0f64ae75e83c9a524d892ff
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-17 16:11:59 +1100

    Added class GradeStatistics

--- csharp_grades_001/GradeBook.cs
+++ csharp_grades_001/GradeBook.cs
@@ -13,6 +13,22 @@ namespace csharp_grades_001
             grades = new List<float>();
         }
 
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            stats.HighestGrade = 0;
+
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+        }
+
         public void AddGrade(float grade)
         {
             grades.Add(grade);
