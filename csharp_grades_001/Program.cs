b49b102c665757f968a7b58a4f4aad8a3fa1258e
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-17 14:42:20 +1100

    First commit

---
+++ csharp_grades_001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_grades_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
